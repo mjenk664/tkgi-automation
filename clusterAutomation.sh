a58fd90db7219f3b499d4590f96cfa890c555d13
Matt Jenkins <matthew.jenkins@altoros.com>
2021-03-05 16:46:48 -0600

    fix logic errors/syntax - add update/upgrade functions

--- clusterAutomation.sh
+++ clusterAutomation.sh
@@ -53,14 +53,14 @@ delete_cluster() {
 }
 
 existing_num_nodes() {
-    local -r c=$1
-    echo $c | jq -r '.parameters.kubernetes_worker_instances'
+    local -r c=${1}
+    echo "${c}" | jq '.parameters.kubernetes_worker_instances' -r
 }
 
 external_hostname() {
     local -r cc=$1
 
-    if [[ "$(grep external_hostname "$cc")" == "" ]]; then
+    if [[ "$(cat $cc | grep -e "external_hostname")" == "" ]]; then
        # Error out if External Hostname is not defined in config
        echo "No External Hostname defined in $cc...Aborting!"
        echo "Please define an External Hostname in $cc"
@@ -74,7 +74,7 @@ external_hostname() {
 plan() {
     local -r cc=$1
 
-    if [[ "$(grep plan "$cc")" == "" ]]; then
+    if [[ "$(cat $cc | grep -e "plan")" == "" ]]; then
        # Error out if Plan is not defined in config
        echo "No Plan defined in $cc...Aborting!"
        echo "Please define a Plan in $cc"
@@ -88,43 +88,30 @@ plan() {
 network_profile() {
     local -r cc=$1
 
-    if [[ "$(grep network_profile "$cc")" == "" ]]; then
+    if [[ "$(cat $cc | grep -e "network_profile")" == "" ]]; then
        # Error out if NP is not defined in config
        echo "No Network Profile defined in $cc...Aborting!"
        echo "Please define a Network Profile in $cc"
        exit 1
     else
        # return NP name from config
-       np=$(yqx '.network_profile' "$cc")
-       echo "--network-profile $np"
+       np_check=$(yqx '.network_profile' "$cc")
+       echo "--network-profile $np_check"
     fi
 }
 
 kubernetes_profile() {
     local -r cc=$1
 
-    if [[ "$(grep kubernetes_profile "$cc")" == "" ]]; then
+    if [[ "$(cat $cc | grep -e "kubernetes_profile")" == "" ]]; then
        # Error out if KP is not defined in config
        echo "No Kubernetes Profile defined in $cc...Aborting!"
        echo "Please define a Kubernetes Profile in $cc"
        exit 1
     else
        # return KP name from config
-       kp=$(yqx '.kubernetes_profile' "$cc")
-       echo "--kubernetes-profile $kp"
-    fi
-}
-
-compute_profile() {
-    local -r cc=$1
-
-    if [[ "$(grep compute_profile "$cc")" == "" ]]; then
-       # Error out if KP is not defined in config
-       echo "No Compute Profile defined in $cc...skipping"
-    else
-       # return CP name from config
-       cp=$(yqx '.compute_profile' "$cc")
-       echo "--compute-profile $cp"
+       kp_check=$(yqx '.kubernetes_profile' "$cc")
+       echo "--kubernetes-profile $kp_check"
     fi
 }
 
@@ -132,7 +119,7 @@ check_kubernetes_profile_exists() {
     local -r cluster=$1
     local -r cluster_dir=$2
     local -r cc=$(cluster_config $cluster $cluster_dir)
-    local -r kp_name=$(kubernetes_profile $cc)
+    local -r kp_name=$(yqx '.kubernetes_profile' $cc)
 
     # check if KP has been created in TKGI
     kp_exists=$(tkgi kubernetes-profiles --json | jq '.[] | .name' -rc)
@@ -147,6 +134,109 @@ check_kubernetes_profile_exists() {
     else
       echo "Kubernetes Profile: $kp_name found in TKGI!"
     fi
+}
+
+compute_profile() {
+    local -r cc=$1
+
+    if [[ "$(cat $cc | grep -e "compute_profile")" != "" ]]; then
+       # return CP name from config
+       cp_check=$(yqx '.compute_profile' "$cc")
+       echo "--compute-profile $cp_check"
+    fi
+}
+
+check_compute_profile_exists() {
+    local -r cluster=$1
+    local -r cluster_dir=$2
+    local -r cc=$(cluster_config $cluster $cluster_dir)
+    local -r cp_name=$(yqx '.compute_profile' $cc)
+
+    # check if CP has been created in TKGI
+    cp_exists=$(tkgi compute-profiles --json | jq '.[] | .name' -rc)
+
+    # search in the common directory for the profile name
+    if [[ "$(echo "$cp_exists" | grep "$cp_name")" == "" ]]; then
+      echo -e "Error - Compute Profile: $cp_name does not exist in TKGI"
+      echo "Attemping to create the Compute Profile: $cp_name"
+      echo "Searching in profile in $cluster_dir directory"
+      cp_file_path=$(find $cluster_dir -name *$cp_name*)
+      tkgi create-compute-profile "${cp_file_path}"
+    else
+      echo "Compute Profile: $cp_name found in TKGI!"
+    fi
+}
+
+upgrade_cluster() {
+  local -r cluster=$1
+  local -r cluster_dir=$2
+  local -r cc=$(cluster_config $cluster $cluster_dir)
+  echo "cluster_config = ${cc}"
+
+  # run upgrade-cluster
+  echo "Running tkgi upgrade-cluster $cluster"
+  run \
+     tkgi \
+     upgrade-cluster $cluster \
+     --non-interactive
+}
+
+update_cluster() {
+  local -r cluster=$1
+  local -r cluster_dir=$2
+  local -r cc=$(cluster_config $cluster $cluster_dir)
+  echo "cluster_config = ${cc}"
+  local -r tnn=$(yqx .num_nodes $cc)
+  local -r c=$(cluster_data $cluster)
+  local -r ehn=$(external_hostname $cc)
+  local -r p=$(plan $cc)
+  local -r np=$(network_profile $cc)
+  local -r kp=$(kubernetes_profile $cc)
+  local -r cp=$(compute_profile $cc)
+
+  if [ ! -z "$c" ];
+  then
+      # if a compute profile exists, then
+      # update cluster w/ --compute-profile
+      if [ ! -z "$cp" ];
+      then
+        echo update $cluster \
+             with external hostname $ehn \
+             using plan $p \
+             and profile flags... \
+             $np \
+             $kp \
+             $cp
+        run \
+            tkgi \
+            update-cluster $cluster \
+            --external-hostname $ehn \
+            --plan $p \
+            --num-nodes $tnn \
+            $np \
+            $kp \
+            $cp \
+            --wait
+      else
+        echo updating $cluster \
+             with external hostname $ehn \
+             using plan $p \
+             and profile flags... \
+             $np \
+             $kp
+        run \
+            tkgi \
+            update-cluster $cluster \
+            --external-hostname $ehn \
+            --plan $p \
+            --num-nodes $tnn \
+            $np \
+            $kp \
+            --wait
+      fi
+  fi
+
+  echo "Cluster Details: $(cluster_details $cluster)"
 }
 
 converge_cluster() {
@@ -164,7 +254,7 @@ converge_cluster() {
 
   if [ ! -z "$c" ];
   then
-      local -r enn=$(existing_num_nodes $c)
+      local -r enn=$(existing_num_nodes "${c}")
 
       if [ ! -z "$cp" ];
       then
@@ -185,27 +275,47 @@ converge_cluster() {
          fi
       fi
   else
-      echo constructing $cluster \
-           with external hostname $ehn \
-           using plan $p \
-           and profile flags... \
-           $np \
-           $kp \
-           $cp
-      run \
-          tkgi \
-          create-cluster $cluster \
-          --external-hostname $ehn \
-          --plan $p \
-          --num-nodes $tnn \
-          $np \
-          $kp \
-          $cp \
-          --wait
+      # if a compute profile exists, then
+      # create cluster w/ --compute-profile
+      if [ ! -z "$cp" ];
+      then
+        echo constructing $cluster \
+             with external hostname $ehn \
+             using plan $p \
+             and profile flags... \
+             $np \
+             $kp \
+             $cp
+        run \
+            tkgi \
+            create-cluster $cluster \
+            --external-hostname $ehn \
+            --plan $p \
+            --num-nodes $tnn \
+            $np \
+            $kp \
+            $cp \
+            --wait
+      else
+        echo constructing $cluster \
+             with external hostname $ehn \
+             using plan $p \
+             and profile flags... \
+             $np \
+             $kp
+        run \
+            tkgi \
+            create-cluster $cluster \
+            --external-hostname $ehn \
+            --plan $p \
+            --num-nodes $tnn \
+            $np \
+            $kp \
+            --wait
+      fi
   fi
 
-  echo "Cluster Details:"
-  $(cluster_details $cluster)
+  echo "Cluster Details: $(cluster_details $cluster)"
 
   # run tkgi get-credentials "$cluster"
   # kubectl config set-context "$cluster"
@@ -223,9 +333,28 @@ converge() {
       if [ -d "$dir" ];
       then
           cluster=$(basename "${dir}")
-          check_kubernetes_profile_exists $cluster $cluster_dir
           echo converging cluster $cluster...
           run converge_cluster $cluster $cluster_dir
+      else
+          echo skipping $dir...
+      fi
+  done
+  echo done
+}
+
+# upgrade all clusters in parallel
+upgrade_all_clusters() {
+  local -r cluster_dir=$1
+
+  echo iterating through cluster definitions in $cluster_dir...
+  local cluster
+
+  for dir in ${cluster_dir}/*; do
+      if [ -d "$dir" ];
+      then
+          cluster=$(basename "${dir}")
+          echo upgrading cluster $cluster...
+          run upgrade_cluster $cluster $cluster_dir
       else
           echo skipping $dir...
       fi
